71572146b473b2de43b248edf2437e7bbbbd64b0
chan0139 <33649931+chan0139@users.noreply.github.com>
2021-07-10 17:55:42 +0900

    3장

--- users/models.py
+++ users/models.py
@@ -1,3 +1,42 @@
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):  # AbstractUser class 상속
+    """ Custom User Model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES  = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+    
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_ENGLISH = "en"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_KOREAN, "Kr"),
+        (LANGUAGE_ENGLISH, "En"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "Usd"),
+        (CURRENCY_KRW, "Krw"),
+    )
+    
+    avatar = models.ImageField(blank = True) #blank는 form , null 은 DB에 적용
+    gender = models.CharField(choices =GENDER_CHOICES,max_length=10, blank = True)
+    bio = models.TextField(blank = True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices =LANGUAGE_CHOICES,max_length=10, blank = True)
+    currency = models.CharField(choices =CURRENCY_CHOICES,max_length=10, blank = True)
+    superhost = models.BooleanField(default=False)
