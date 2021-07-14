b12e5b6fd49a381f924ef6e30eebd8301586f0b7
chan0139 <33649931+chan0139@users.noreply.github.com>
2021-07-15 02:54:28 +0900

    5.3

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from core import models as core_models
 
-# Create your models here.
+class Reservation(core_models.TimeStampedModel):
+
+    """ Reservation Model Definition"""
+
+    STATUS_PENDING = "pending"
+    STATUS_CONFIRMED = "confirmed"
+    STATUS_CANCELED = "canceled"
+
+    STATUS_CHOICES = (
+        (STATUS_PENDING, "pending"),
+        (STATUS_CONFIRMED, "confirmed"),
+        (STATUS_CANCELED, "canceled"),
+    )
+
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default=STATUS_PENDING)
+    guest = models.ForeignKey("users.User", on_delete=CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    def __str__(self):
+        return f'{self.room} - {self.check_in}'
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from core import models as core_models
+class Review(core_models.TimeStampedModel):
 
-# Create your models here.
+    """ Review Model Definition"""
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    clearliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE) # 리뷰하는 사람
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE) # 리뷰하는 방
+
+    def __str__(self):
+        return f'{self.review} - {self.room}' 
