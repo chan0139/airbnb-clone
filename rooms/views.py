3c1630611f2c718778be452aafa95538f949c597
chan0139 <33649931+chan0139@users.noreply.github.com>
2021-07-26 17:02:38 +0900

    11.3

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,19 @@
+from math import ceil
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
 
 def all_rooms(request):
-    rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"potato": all_rooms})
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size*page
+    offset = limit-page_size
+    rooms = models.Room.objects.all()[offset:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(request, "rooms/home.html", context={
+                                                "potato": rooms,
+                                                 "page": page,
+                                                 "page_count": page_count,
+                                                 "page_range": range(1, page_count+1),
+                                                 })
