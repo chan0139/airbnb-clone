8a28311f7014f406741c8255ef9a509b92af9d93
chan0139 <33649931+chan0139@users.noreply.github.com>
2021-07-27 03:24:02 +0900

    11.8

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,24 @@
-from math import ceil
-from django.shortcuts import render
-from django.http import HttpResponse
+#from math import ceil
+#from django.shortcuts import redirect, render
+#from django.http import HttpResponse
+#from django.core.paginator import EmptyPage, Paginator
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
+class HomeView(ListView):
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+"""
 def all_rooms(request):
     page = request.GET.get("page", 1)
     page = int(page or 1)
@@ -17,3 +33,19 @@ def all_rooms(request):
                                                  "page_count": page_count,
                                                  "page_range": range(1, page_count+1),
                                                  })
+""" # paginator 수동 방법
+
+"""
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page":rooms})
+    except EmptyPage:
+        return redirect("/")
+    #print(dir(rooms)) paginator func 확인 
+""" # paginator 자동 방법
+
+
