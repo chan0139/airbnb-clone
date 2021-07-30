656929ec597891f0adf53b7f7c1d2a6ee07246dc
chan0139 <33649931+chan0139@users.noreply.github.com>
2021-07-30 22:38:03 +0900

    13.4

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,4 +6,4 @@ app_name = "rooms"
 
 urlpatterns = [path("<int:pk>", views.RoomDetail.as_view(), name = "detail"),
                path("search/", views.search, name = "search"),
-]   
+]  
--- rooms/views.py
+++ rooms/views.py
@@ -1,73 +1,57 @@
-#from math import ceil
-from django.shortcuts import redirect, render
-from django.urls.base import reverse
-from django.http import Http404 
-#from django.http import HttpResponse
-#from django.core.paginator import EmptyPage, Paginator
-from django.utils import timezone
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
-
-class HomeView(ListView): # HOMEVIEW CBV
+class HomeView(ListView):
+    """ HomeView Definition """
     model = models.Room
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
-
 class RoomDetail(DetailView):
-
+    """ RoomDetail Definition """
     model = models.Room
-
+    
 def search(request):
-    #print(request)
-    city = request.GET.get("city")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city":city})
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
 
-"""
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room":room})
-    except models.Room.DoesNotExist:
-        raise Http404() # when Debug = false -> 404.html open
-""" # DETAIL VIEW FBV
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
 
-"""
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size*page
-    offset = limit-page_size
-    rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(request, "rooms/home.html", context={
-                                                "potato": rooms,
-                                                 "page": page,
-                                                 "page_count": page_count,
-                                                 "page_range": range(1, page_count+1),
-                                                 })
-""" # paginator 수동 방법
-
-"""
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page":rooms})
-    except EmptyPage:
-        return redirect("/")
-    #print(dir(rooms)) paginator func 확인 
-""" # paginator 자동 방법 FBV HOMEVIEW
-
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+    return render(request, "rooms/search.html", {**form, **choices})
 
